57decad5a45bb96854af5e9872752ab066f386b0
Ishrith Gowda <ishrithg@gmail.com>
2025-08-03 11:38:49 -0500

    modified the testing name and description, and reformatted some of the programs

--- frontend/src/components/MealCard.test.jsx
+++ frontend/src/components/MealCard.test.jsx
@@ -6,16 +6,16 @@ import MealCard from "./MealCard";
 
 
 const mockMeal = {
-  name: "Grilled Chicken Salad",
-  description: "Fresh greens with grilled chicken, cherry tomatoes, and vinaigrette",
-  calories: 350
+  name: "sample name",
+  description: "sample description",
+  calories: 1
 };
 
 test("renders MealCard with correct info", () =>
 {
   render(<MealCard meal={mockMeal} />);
   
-  expect(screen.getByText(/grilled chicken salad/i)).toBeInTheDocument();
-  expect(screen.getByText(/fresh greens with grilled chicken/i)).toBeInTheDocument();
-  expect(screen.getByText(/calories: 350/i)).toBeInTheDocument();
+  expect(screen.getByText(/same name/i)).toBeInTheDocument();
+  expect(screen.getByText(/sample description/i)).toBeInTheDocument();
+  expect(screen.getByText(/calories: 1/i)).toBeInTheDocument();
 });
