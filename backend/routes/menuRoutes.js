494cced0fd91fc110ae0419f31742dfa4546fa7f
Ishrith Gowda <ishrithg@gmail.com>
2025-08-07 12:23:46 -0500

    modified /routes/menus.js because currently uses commonjs but project uses es modules

--- backend/routes/menuRoutes.js
+++ backend/routes/menuRoutes.js
@@ -1,10 +1,11 @@
-const express = require('express');
+// backend/routes/menuRoutes.js
+import express from "express";
+
 const router = express.Router();
 
 // GET /api/menus/sync
-router.get('/sync', (req, res) =>
-{
+router.get('/sync', (req, res) => {
   res.json({ message: 'menu sync route is working' });
 });
 
-module.exports = router;
+export default router;
